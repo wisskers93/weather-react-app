0ed983daad619e3ef0b90792cbbbf0bd624f5599
Wisskers93 <cmwgonzales18@gmail.com>
2020-06-08 13:11:07 -0400

    changed title

--- weather-app/src/components/Titles.js
+++ weather-app/src/components/Titles.js
@@ -4,8 +4,8 @@ class Titles extends React.Component{
     render(){
         return(
             <div>
-                <h1>Weather Finder</h1>
-                <p>Find out temperature, conditions, and more...</p>
+                <h1>Your Weather App</h1>
+                <p>Find out your local weather conditions</p>
             </div>
 
         );
